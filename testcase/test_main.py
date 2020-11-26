647c612d17346a3e4295e31d8f5f1df36082b4c3
hao.fan <286305546@qq.com>
2020-11-26 18:11:00 +0800

    更新ordercase

--- testcase/test_main.py
+++ testcase/test_main.py
@@ -40,8 +40,20 @@ class Test_main:
         actions.press(x=348,y=1132).wait(1000).move_to(x=348,y=322).wait(1000).release().perform()
         WebDriverWait(self.driver, 10).until(
             expected_conditions.element_to_be_clickable((MobileBy.XPATH, '(//android.view.View[@content-desc="购买"])[3]')))
-        self.driver.find_element(MobileBy.XPATH,'(//android.view.View[@content-desc="购买"])[3]').click()
+        self.driver.find_element(MobileBy.XPATH,'(//android.view.View[@content-desc="购买"])[2]').click()
         #下单
         self.driver.find_element(MobileBy.XPATH,'//*[@resource-id="com.yckj.eshop:id/tvOperatingLess"]').click()
+        #点击购买数量-号
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='-' and @resource-id='com.yckj.eshop:id/tvOperatingLess']").click()
+        print(self.driver.page_source)
+        toastele=self.driver.find_element(MobileBy.XPATH,"//*[contains(@text,'已是最小预订数')]")
+        print(toastele.get_attribute("text"))
+        toasttext=toastele.get_attribute("text")
+        assert toasttext == "已是最小预订数"
+        #输入信息下单
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='请填写姓名']").send_keys('浩然')
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='请填写手机号码']").send_keys('15009253686')
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='请填写证件号码']").send_keys('610102199112241910')
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='立即预订']").click()
 
 
